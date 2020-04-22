6ee4f2641d629cf38e8b507546992bb03f5dfe41
WendaLi <lwd19861127@hotmail.com>
2020-04-22 13:25:10 +0800

    Assignment3

---
+++ StoriesNavigationController/StoriesNavigationController/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  StoriesNavigationController
+//
+//  Created by WendaLi on 2020-04-22.
+//  Copyright © 2020 WendaLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToFrontDoor(unwindSegue: UIStoryboardSegue) {
+    }
+}
+
